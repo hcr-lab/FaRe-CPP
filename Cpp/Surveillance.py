5714a232415076e03230735df47926318a3382b4
sriapple <sk65@tu_clausthal.de>
2024-09-13 23:31:20 +0200

    cleaned script

--- Cpp/Surveillance.py
+++ Cpp/Surveillance.py
@@ -1,8 +1,8 @@
-from FireBotMAP import Map_generator
+from BotMAP import Map_generator
 import yaml
-from FireBotScout_Multi_Processing import Exploration
+from Scout_Multi_Processing import Exploration
 from path_generator import calculate_and_plot_path
-from FireBot_path_optimizer import WayPointOptimizer
+from path_optimizer import WayPointOptimizer
 from config import config 
 import os
 def main():
@@ -26,9 +26,9 @@ def main():
 
     #map_generator.plot_rgb_map(grid_map)
 
-    explore = Exploration(grid_map, s_range, unexplored_value, state, yaml_data)
+    explore = Exploration(grid_map, s_range, unexplored_value, state, yaml_data) #intializes the  way point generation object
 
-    goals = explore.set_goals(starting_position, explored_value, unexplored_value, steps,WPD)
+    goals = explore.set_goals(starting_position, explored_value, unexplored_value, steps,WPD) # waypoints generation returns list of waypoints(cor_x, cor_y) and orientation theta
     #map_generator.plot_iterations(goals)
     wp = []
     ori = []
@@ -50,10 +50,10 @@ def main():
     with open(file_path, 'w') as f:
         f.write(f"wp = {wp}\n")
         f.write(f"ori = {ori}\n")
-    t_c_rot = sum(ori)
-    resolution = yaml_data['resolution']
-    optimizer = WayPointOptimizer(wp, 0.3, 5)
-    best_wp =optimizer.run()
+    t_c_rot = sum(ori) # total cummulative rotations
+    resolution = yaml_data['resolution'] # resolution of grid cell
+    optimizer = WayPointOptimizer(wp, 0.3, 5) # initializes way point optimization object
+    best_wp =optimizer.run() #optimizes waypoints
     metrics = calculate_and_plot_path(grid_map,wp,best_wp,resolution,t_c_rot,output_dir)
     print(metrics)
     
